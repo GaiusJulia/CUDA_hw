a79c23f5a9fe11b1daa218fe15866baa1468923a
GaiusJulia <114506257+GaiusJulia@users.noreply.github.com>
2023-05-18 13:40:52 +0300

    Add files via upload

--- hw4/hw4.cu
+++ hw4/hw4.cu
@@ -1,1 +1,87 @@
-coming soon...
+#include<stdio.h>
+#include<iostream>
+
+// Задание 4: Перемножение двух матриц
+__global__
+void ArrMul(int mul_size, float* A, float* B, float* res){
+    int tid_x = blockDim.x * blockIdx.x + threadIdx.x; // нить отвечает за элемент массива res
+    int tid_y = blockDim.y * blockIdx.y + threadIdx.y;
+    int width = blockDim.y * gridDim.y;
+
+    int index = tid_x * width + tid_y;
+    for (int i = 0; i < mul_size; i ++){ // свертка по строке tid_x из массива A и столбцу tid_y из массива B
+        res[index] = A[tid_x * mul_size + i] * B[i * width + tid_y];
+    }
+}
+
+int main() {
+    //step 1
+    int n = 128;
+    int m = 64;
+    int k = 128;
+    float* h_A = new float[n*m];
+    float* h_B = new float[m*k];
+    float* h_res = new float[n*k];
+
+    //step 2
+    float* d_A;
+    float* d_B;
+    float* d_res;
+    int nbytes = n * m * sizeof(float);
+    cudaMalloc(&d_A, nbytes);
+    cudaMalloc(&d_B, nbytes);
+    cudaMalloc(&d_res, nbytes);
+
+    //
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            h_A[i*m + j] = 1;
+            h_B[i*m + j] = 2;
+        }
+    }
+
+    //step 3
+    cudaMemcpy(d_A, h_A, nbytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, nbytes, cudaMemcpyHostToDevice);
+
+    cudaEvent_t start, end;
+    cudaEventCreate(&start);
+    cudaEventCreate(&end);
+
+    //step 4
+    cudaEventRecord(start);
+    dim3 num_blocks(16, 16);
+    dim3 block_size(8, 8);
+    // необходимо num_blocks.x * block_size.x = n,  num_blocks.y * block_size.y = k
+    ArrMul<<<num_blocks, block_size>>>(m, d_A, d_B, d_res);
+
+    cudaEventRecord(end);
+
+    //step 5
+    cudaMemcpy(h_res, d_res, nbytes, cudaMemcpyDeviceToHost);
+    cudaEventSynchronize(end);
+
+    float ms;
+    cudaEventElapsedTime(&ms, start, end);
+    std::cout << "Time elapsed: " << ms << " ms " << std::endl;
+
+    /*
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            std::cout << h_res[i*m + j] << " ";
+        std::cout << std::endl;
+    }
+    //*/
+
+    //step 6
+    delete[] h_A;
+    delete[] h_B;
+    delete[] h_res;
+    //step 7
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_res);
+}
